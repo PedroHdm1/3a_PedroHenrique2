45900b32e71e1e03db0e8838af677a9f63cb45b6
04262487245 <04262487245@JIPA-LABIN02-20.ifro.local>
2018-05-02 14:34:55 -0400

    Mapeie o banco de dados, criei a tela de cadastro e gravei no banco

--- PedroHenrique/src/br/edu/ifro/FXMLDocumentController.java
+++ PedroHenrique/src/br/edu/ifro/FXMLDocumentController.java
@@ -5,31 +5,63 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
+ * FXML Controller class
  *
  * @author 04262487245
  */
 public class FXMLDocumentController implements Initializable {
-    
+
+    @FXML
+    private Button button;
     @FXML
     private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load(),900,682);
+                    Stage stage = new Stage();
+            stage.setTitle("Cadastrar ALuno");
+            stage.setScene(scene);
+            stage.show();
+        }
+    catch(IOException e){
+
+    }
+        
+    }
+
+    @FXML
+    private void fecharProjeto(ActionEvent event) {
+    }
+
+
     
 }
